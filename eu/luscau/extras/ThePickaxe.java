1f94de153ae5c9030ddcc59ecbc56e7463c24c19
Luscau <lucascavalcante009@hotmail.com>
2017-08-27 00:46:14 -0300

    Update ThePickaxe.java

--- eu/luscau/extras/ThePickaxe.java
+++ eu/luscau/extras/ThePickaxe.java
@@ -1,40 +1,35 @@
-/*    */ package eu.luscau.extras;
-/*    */ 
-/*    */ import eu.luscau.PickBreakBedRock;
-/*    */ import java.util.ArrayList;
-/*    */ import java.util.List;
-/*    */ import org.bukkit.Material;
-/*    */ import org.bukkit.configuration.file.FileConfiguration;
-/*    */ import org.bukkit.enchantments.Enchantment;
-/*    */ import org.bukkit.inventory.ItemStack;
-/*    */ import org.bukkit.inventory.meta.ItemMeta;
-/*    */ import org.bukkit.plugin.Plugin;
-/*    */ 
-/*    */ public class ThePickaxe
-/*    */ {
-/* 15 */   private static String NAME = PickBreakBedRock.getPlugin().getConfig().getString("Pickaxe.NAME");
-/* 16 */   private static List<String> LORE = PickBreakBedRock.getPlugin().getConfig()
-/* 17 */     .getStringList("Pickaxe.LORE".replace("&", "§"));
-/*    */   
-/*    */   public static ItemStack getPickaxe() {
-/* 20 */     ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
-/*    */     
-/* 22 */     ItemMeta meta = item.getItemMeta();
-/* 23 */     meta.setDisplayName(NAME.replace("&", "§"));
-/* 24 */     meta.addEnchant(Enchantment.SILK_TOUCH, 2, true);
-/* 25 */     List<String> lore = new ArrayList();
-/*    */     
-/* 27 */     for (String string : LORE) {
-/* 28 */       lore.add(string.replace("&", "§"));
-/*    */     }
-/* 30 */     meta.setLore(lore);
-/* 31 */     item.setItemMeta(meta);
-/* 32 */     return item;
-/*    */   }
-/*    */ }
+package eu.luscau.extras;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/* Location:              C:\Users\Lucas\Desktop\testar pls\plugins\lcPickBreakBedRock.jar!\eu\luscau\extras\ThePickaxe.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import eu.luscau.PickBreakBedRock;
+
+public class ThePickaxe {
+
+	private static String NAME = PickBreakBedRock.getPlugin().getConfig().getString("Pickaxe.NAME");
+	private static List<String> LORE = (List<String>) PickBreakBedRock.getPlugin().getConfig()
+			.getStringList("Pickaxe.LORE".replace("&", "§"));
+
+	public static ItemStack getPickaxe() {
+		ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
+
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ThePickaxe.NAME.replace("&", "§"));
+		meta.addEnchant(Enchantment.SILK_TOUCH, 2, true);
+		List<String> lore = new ArrayList<String>();
+
+		for (String string : ThePickaxe.LORE) {
+			lore.add(string.replace("&", "§"));
+		}
+		meta.setLore(lore);
+		item.setItemMeta(meta);
+		return item;
+	}
+
+}
